2efb994655d5a59fb930dd74990c7a41165c15c1
Marco Mechini <mechinimarco35@gmail.com>
2025-02-07 10:48:46 +0100

    feat: creazione rotte

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 import { useState } from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 function App() {
 
   return (
     <>
-
+      <BrowserRouter>
+        <Routes element={<AppLayout />}>
+          <Route>
+            <Route index element={<HomePage />}></Route>
+            <Route path='/Dettaglio'>
+              <Route index element={<PaginaRicerca />}></Route>
+              <Route path=':id' element={<PaginaDettaglio />}></Route>
+            </Route>
+            <Route path='/Inserimento' element={<PaginaInserimento />}></Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
