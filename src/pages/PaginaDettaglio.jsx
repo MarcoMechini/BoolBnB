576c261f6eb81646eda6649613ddc743eb35700a
BartCatalano <catalanobartolomeo@gmail.com>
2025-02-10 17:41:33 +0100

    feat: form per le recensioni fatto

--- src/pages/PaginaDettaglio.jsx
+++ src/pages/PaginaDettaglio.jsx
@@ -1,9 +1,26 @@
+import { useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 
 function PaginaDettaglio() {
-
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [reviewText, setReviewText] = useState('');
     const navigate = useNavigate()
     const { id } = useParams()
+
+
+     // Funzione per gestire l'invio del form
+  const handleSubmit = (e) => {
+    e.preventDefault(); 
+
+    console.log('Recensione inviata:', { name, email, reviewText });
+
+    // Resetta i campi del form dopo l'invio
+    setName('');
+    setEmail('');
+    setReviewText('');
+  };
+
 
 
 
@@ -11,6 +28,47 @@ function PaginaDettaglio() {
         <>
         <button onClick={() => navigate(-1)}>Indietro</button>
             <h3>sono pagina dettaglio {id}</h3>
+        
+            <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="name">Nome:</label>
+        <input
+          type="text"
+          id="name"
+          name="name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          required
+        />
+      </div>
+
+      <div>
+        <label htmlFor="email">Email:</label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+      </div>
+
+      <div>
+        <label htmlFor="reviewText">Recensione:</label>
+        <textarea
+          id="reviewText"
+          value={reviewText}
+          name="reviewText"
+          onChange={(e) => setReviewText(e.target.value)}
+          required
+        />
+      </div>
+
+      <button type="submit">Invia Recensione</button>
+    </form>
+
+
         </>
     )
 
