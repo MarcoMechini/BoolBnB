c56c1fdcfb40eed4f963aae36da0d338b07ae5a3
BartCatalano <catalanobartolomeo@gmail.com>
2025-02-10 17:22:07 +0100

    feat: tasto indietro inserito nella pagina dettagli

--- src/pages/PaginaDettaglio.jsx
+++ src/pages/PaginaDettaglio.jsx
@@ -1,9 +1,15 @@
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 
 function PaginaDettaglio() {
+
+    const navigate = useNavigate()
     const { id } = useParams()
+
+
+
     return (
         <>
+        <button onClick={() => navigate(-1)}>Indietro</button>
             <h3>sono pagina dettaglio {id}</h3>
         </>
     )
