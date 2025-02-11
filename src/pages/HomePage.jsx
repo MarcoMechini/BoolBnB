873a6a4552478cb1e05be6093025c71f51821397
BartCatalano <catalanobartolomeo@gmail.com>
2025-02-11 12:09:11 +0100

    feat: inseriti i dati per la card delle case

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,70 +4,81 @@ import { Link, useNavigate } from "react-router-dom";
 
 function HomePage() {
 
-// CREO LO USE STATE PER LA SEARCHBAR
-const [search, setSearch] = useState("");
-const [filtroCittà, setFiltroCittà] = useState([]);
-const navigate = useNavigate();
+    // CREO LO USE STATE PER LA SEARCHBAR
+    const [search, setSearch] = useState("");
+    const [filtroCittà, setFiltroCittà] = useState([]);
+    const navigate = useNavigate();
 
-useEffect(()=>{
-    caricamentoDati();
-},[]);
+    useEffect(() => {
+        caricamentoDati();
+    }, []);
 
-const caricamentoDati = () => {
-    axios.get('http://localhost:4000/boolbnb').then((resp) => {
-        console.log(resp.data.data);
-        setFiltroCittà(resp.data.data);
-    })
-  };
-
-
-
-// filtro le città in base al termine di ricerca
-const ricercaManuale = () => { 
-    const filtro = filtroCittà.filter(casa =>
-    casa.city.toLocaleLowerCase().includes(search.toLocaleLowerCase())
-);
-setFiltroCittà(filtro)
-setSearch("");
-
-navigate("/Ricerca" ,{ state: {filtroCittà: filtro}});
-};
+    const caricamentoDati = () => {
+        axios.get('http://localhost:4000/boolbnb').then((resp) => {
+            console.log(resp.data.data);
+            setFiltroCittà(resp.data.data);
+        })
+    };
 
 
 
+    // filtro le città in base al termine di ricerca
+    const ricercaManuale = () => {
+        const filtro = filtroCittà.filter(casa =>
+            casa.city.toLocaleLowerCase().includes(search.toLocaleLowerCase())
+        );
+        setFiltroCittà(filtro)
+        setSearch("");
+
+        navigate("/Ricerca", { state: { filtroCittà: filtro } });
+    };
 
 
-return(
-<>
-<section className="container">
-<h1>Home Page</h1>
-
-{/* barra di ricerca */}
-<input type="text"
-placeholder="Cerca per città"
-value={search}
-onChange={(e) => setSearch(e.target.value)} />
-{/* inserisco bottone ricerca */}
-
-<button type="button" onClick={ricercaManuale}>Cerca</button>
 
 
-{/* risultato ricerca */}
 
-{filtroCittà.length > 0 ? (
-    filtroCittà.map((curCasa) =>(
-        // qui dentro svilupperò la card delle case
-        
-        <div key={curCasa.id}><Link to={`/Ricerca/${curCasa.id}`}>{curCasa.city} - {curCasa.title}</Link> </div>
-    ))
-) : ( <p>nessuna casa trovata in questa città</p> ) }
+    return (
+        <>
+            <section className="container">
+                <h1>Home Page</h1>
+
+                {/* barra di ricerca */}
+                <input type="text"
+                    placeholder="Cerca per città"
+                    value={search}
+                    onChange={(e) => setSearch(e.target.value)} />
+                {/* inserisco bottone ricerca */}
+
+                <button type="button" onClick={ricercaManuale}>Cerca</button>
 
 
-</section>
+                {/* risultato ricerca */}
 
-</>
+                {filtroCittà.length > 0 ? (
+                    filtroCittà.map((curCasa) => (
+                        // qui dentro svilupperò la card delle case
+                        <div key={curCasa.id}>
+                            <div>{curCasa.city}</div>
+                            <div>{curCasa.address}</div>
+                            <div>{curCasa.title}</div>
+                            <div>{curCasa.descr}</div>
+                            <div>Stanze:{curCasa.rooms}</div>
+                            <div>Bagni:{curCasa.bathrooms}</div>
+                            <div>Camere da letto: {curCasa.bedrooms}</div>
+                            <div>{curCasa.squere_meters}</div>
+                            <div>Like{curCasa.likes}</div>
+                            <div>{curCasa.url_img}</div>
+                            <Link to={`/Ricerca/${curCasa.id}`}>INFO</Link> 
+                            </div>
+                    ))
+                ) : (<p>nessuna casa trovata in questa città</p>)}
 
-)
+
+            </section>
+
+        </>
+
+    )
 
 
 };
